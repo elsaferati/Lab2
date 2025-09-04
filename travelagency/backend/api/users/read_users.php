90b098efd6fb6f5103f4c8998556fd3402b9ed41
deabahtirii <deabahtiri.it@gmail.com>
2025-09-05 01:14:18 +0200

    Create read_users.php

---
+++ travelagency/backend/api/users/read_users.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__.'/../../utils/cors.php';
+require_once __DIR__.'/../../utils/response.php';
+require_once __DIR__.'/../../db.php';
+require_once __DIR__.'/../../models/UserModel.php';
+
+handle_preflight(); send_cors_headers();
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') err(405,'Method not allowed');
+
+$users = new UserModel($mysqli);
+
+if(isset($_GET['id'])){
+  $id = (int)$_GET['id'];
+  if($id<=0) err(400,'Invalid id');
+  $u = $users->getById($id);
+  if(!$u) err(404,'Not found');
+  ok(['user'=>$u]);
+} else {
+  $list = $users->listAll();
+  ok(['users'=>$list]);
+}
